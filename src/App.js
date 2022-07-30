a3c8f93c102f33f9bc509b9e33a333373177bf49
WMCharles <charles.wafula@student.moringaschool.com>
2022-07-30 20:09:05 +0300

    xxx

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' exact element={<Home/>} />
-          <Route path='/services' element={<Services/>} />
+          <Route path='services' element={<Services/>} />
           <Route path='/products' element={<Products/>} />
           <Route path='/sign-up' element={<SignUp/>} />
         </Routes>
